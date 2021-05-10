6f7d362538ca967ecc980cb9292df2915b94d505
Pater GitHelp <micbo26@gmail.com>
2021-05-10 20:51:56 +0200

    default login i wartość czy admin
    ja pierdolę, dlaczego railsy nie obsługują intów jak normalny ruby. coś jest kurwa nie tak i indeks po wywołaniu metody jest chuj wie czy i nie działa jak należy
    Ale teraz już jest git

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates_uniqueness_of :login
+
   validates_uniqueness_of :email
   has_many :products, :through=> :user_product
+  after_initialize :default_values, :default_login
+
+  private
+  def default_values
+      self.admin ||= false
+  end
+  def default_login
+   indeks=self.email.index('@')
+   indeks=indeks.to_i-1
+   logg = self.email[0..indeks]
+   self.login ||=logg
+  end
   #enum role: [:admin,:user]
 end
