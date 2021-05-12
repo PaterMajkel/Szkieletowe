6ef2ea1ede332db3d10c4e0a93fa095077f6d2c7
Filip2137 <filip.andrejuk@gmail.com>
2021-05-12 16:55:27 +0200

    Paginacja działa
    Yeahhhh boi

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :users, :through=> :user_product
   has_and_belongs_to_many :shops
+  paginates_per 6
 end
--- app/models/product_shop.rb
+++ app/models/product_shop.rb
@@ -1,5 +1,6 @@
 class ProductShop < ApplicationRecord
   belongs_to :product
   belongs_to :shop
+
 end
 #gitdziala nareszcie
