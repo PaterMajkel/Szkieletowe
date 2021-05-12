be9976da15f840d0d130db159bf9cd59439e1510
Pater GitHelp <micbo26@gmail.com>
2021-05-12 23:57:43 +0200

    model zmiana

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  has_many :users, :through=> :user_product
   has_many :user_product
+  has_many :users, :through=> :user_product
   has_and_belongs_to_many :shops
   paginates_per 6
 end
