1eccfb768134d3bb0baf2add97fd903712b6d275
czarnaherbata <65903006+czarnaherbata@users.noreply.github.com>
2021-05-16 21:01:34 +0200

    search taki nawet lepszy

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -9,7 +9,7 @@ class ProductController < ApplicationController
     end
 
     def search
-      @products = Product.where("name LIKE ?", "%" + params[:q] + "%")
+      @products = Product.where("name LIKE ?", "%" + params[:q] + "%").page(params[:page])
     end
 
     def show
