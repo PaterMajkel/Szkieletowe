7fd0eb2b3fd314cac72dbe5cb5bda78d744f884b
Filip2137 <filip.andrejuk@gmail.com>
2021-05-17 21:30:51 +0200

    Revert "naprawa admina i dodawanie działa"
    This reverts commit 72453d193bdbf229bdb29b70ec94c0388e097331.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-  config.secret_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+  config.site_key = '6Lf3XtkaAAAAAEOdR3yVQll72S5TTxYTJ6eRP1UZ'
+  config.secret_key = '6Lf3XtkaAAAAAO3N0n4dd6chzKWM3f6FcH-KBRan'
 end
