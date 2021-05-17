23668af65b967e3ae85b13d5325032a95c2908e1
Pater GitHelp <micbo26@gmail.com>
2021-05-17 22:25:21 +0200

    Defaultowe wartości Karuzela

--- app/models/homepage_carousel.rb
+++ app/models/homepage_carousel.rb
@@ -1,2 +1,18 @@
 class HomepageCarousel < ApplicationRecord
+
+  after_initialize :default_duration, :default_image, :default_text, unless: :persisted?
+
+  private
+
+  def default_duration
+    self.duration ||= 5000.to_i
+  end
+  def default_image
+    self.image ||= "https://icatcare.org/app/uploads/2018/10/What-is-a-cat.png"
+  end
+
+  def default_text
+    self.heading ||= "Lorem"
+    self.description ||= "Lorem Ipsum"
+  end
 end
