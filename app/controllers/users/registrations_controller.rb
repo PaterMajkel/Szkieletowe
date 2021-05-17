109c321a01068fbb3a41b5b112afe48cc85a53cd
Pater GitHelp <micbo26@gmail.com>
2021-05-17 21:30:10 +0200

    Revert "Revert "Działa recaptcha""
    This reverts commit cb94268308247d5ba1de1a38fdc1668181864821.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,14 @@
-# frozen_string_literal: true
-
-class RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
+class Users::RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :check_captcha, only: :create
 
   private
 
   def check_captcha
-    return if !verify_recaptcha # verify_recaptcha(action: 'signup') for v3
-
-    self.resource = resource_class.new sign_up_params
-    resource.validate # Look for any other validation errors besides reCAPTCHA
-    set_minimum_password_length
-
-    respond_with_navigational(resource) do
-      flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
-      render :new
+    unless verify_recaptcha
+      self.resource = resource_class.new sign_up_params
+      resource.validate
+      set_minimum_password_length
+      respond_with_navigational(resource) { render :new }
     end
   end
 end
