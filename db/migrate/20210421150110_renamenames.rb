db87bb621ac1bbc3f5feb8adb96bf1b5d9676fc8
maciejek052 <maciejek052@gmail.com>
2021-05-25 20:18:19 +0200

    zmiana bazy na postgresa
    żeby działało na windowsie trzeba zainstalować postgresa i PRAWDOPODOBNIE odkomentować dwie linijki w config/database.yml

--- db/migrate/20210421150110_renamenames.rb
+++ db/migrate/20210421150110_renamenames.rb
@@ -2,7 +2,7 @@ class Renamenames < ActiveRecord::Migration[6.1]
   def change
     rename_table :prodshops, :product_shops
     change_column :product_shops, :price, :decimal, :precision => 2
-    change_column :users, :role, :boolean
+    change_column :users, :role, 'boolean USING CAST(role AS boolean)' # nwm o co chodzi ale wyjebywalo blad
     rename_column :users, :role, :admin
     rename_table :user_lists, :user_products
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_13_151425) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "homepage_carousels", force: :cascade do |t|
     t.string "image"
     t.string "alt"
