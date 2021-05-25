6b7347dfe268ad79ee11162d9d32235052915169
Pater GitHelp <micbo26@gmail.com>
2021-05-25 16:48:22 +0200

    Scraper ma wsparcie dla 4 stron

--- app/controllers/productshop_controller.rb
+++ app/controllers/productshop_controller.rb
@@ -1,35 +1,75 @@
 class ProductshopController < ApplicationController
   def create
-    price=999999999
-    @shops=Shop.all
+    price = 999999999
+    @shops = Shop.all
     @shops.each do |shop|
       Product.all.each do |product|
-        case shop.id
-        when 1
-          url="https://mediamarkt.pl/search?query%5Bmenu_item%5D=&query%5Bquerystring%5D=#{product.name.gsub(' ','+')}"
+        case shop.domain
+        when "mediamarkt.pl"
+          url = "https://mediamarkt.pl/search?query%5Bmenu_item%5D=&query%5Bquerystring%5D=#{product.name.gsub(' ', '+')}"
           #url+=product.name.gsub(' ','+')
           doc = Nokogiri::HTML(URI.open(url))
           pricebox = doc.css("div.m-priceBox_price").first
           if pricebox.nil?
-            price=999999999
+            price = 999999999
           else
-            price=pricebox.text.gsub(/\s+/,'')
-            price.gsub(/[()-+.]/,'')
-            price.gsub(/[,]/,'.')
+            price = pricebox.text.gsub(/\s+/, '')
+            price.gsub(/[()-+.]/, '')
+            price.gsub(/[,]/, '.')
             price.tr('^0-9.', '')
-            price=price.to_f
+            price = price.to_f
+          end
+        when "euro.com.pl"
+          url = "https://www.euro.com.pl/search.bhtml?keyword=#{CGI.escape(product.name)}"
+          doc = Nokogiri::HTML(URI.open(url))
+          pricebox = doc.css("div.price-normal.selenium-price-normal").first
+          if pricebox.nil?
+            price = 999999999
+          else
+            price = pricebox.text.gsub(/\s+/, '')
+            price.gsub(/[()-+.]/, '')
+            price.gsub(/[,]/, '.')
+            price.tr('^0-9.', '')
+            price = price.to_f
+          end
+        when "x-kom.pl"
+          url = "https://www.x-kom.pl/szukaj?q=#{product.name.gsub(' ', '%20')}"
+          doc = Nokogiri::HTML(URI.open(url))
+          pricebox = doc.css("span.sc-6n68ef-0.sc-6n68ef-3.hNZEsQ").first
+          if pricebox.nil?
+            price = 999999999
+          else
+            price = pricebox.text.gsub(/\s+/, '')
+            price.gsub(/[()-+.]/, '')
+            price.gsub(/[,]/, '.')
+            price.tr('^0-9.', '')
+            price = price.to_f
+          end
+        when "mediaexpert.pl"
+          url = "https://www.mediaexpert.pl/search?query[menu_item]=&query[querystring]=#{product.name.gsub(' ', '%20')}"
+          doc = Nokogiri::HTML(URI.open(url))
+          pricebox = doc.css("span.a-price_price").first
+          if pricebox.nil?
+            price = 999999999
+          else
+            price = pricebox.text.gsub(/\s+/, '')
+            price.gsub(/[()-+.]/, '')
+            price.gsub(/[,]/, '.')
+            price.tr('^0-9.', '')
+            price = price.to_f
           end
 
-          @productshops=ProductShop.new(shop_id: shop.id, product_id: product.id, price: price, date: Time.now)
-          @productshops.save
+        else
+          next
         end
-
+        #if price != 999999999
+          @productshops = ProductShop.new(shop_id: shop.id, product_id: product.id, price: price, date: Time.now)
+          @productshops.save
+        #end
 
       end
-
     end
     redirect_to request.referrer
+
   end
-
-
 end
