8e434f5b0c42b17bd9f42f38cbbd6e54937fc5a4
Pater GitHelp <micbo26@gmail.com>
2021-05-25 16:56:10 +0200

    Usuwanie Modeli
    Dodałem zależności przy usuwaniu pewnego z modelu, to wszystkie przechodniości też zostaną usunięte

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,5 @@
 class Shop < ApplicationRecord
-  has_many :product_shop
+  has_many :product_shop, :dependent => :destroy
   has_many :products, :through => :product_shop
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates_uniqueness_of :email
-  has_many :user_product
+  has_many :user_product, :dependent => :destroy
   has_many :products, :through=> :user_product
   after_initialize :default_values, :default_login
 
