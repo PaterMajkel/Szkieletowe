4845435aec12c65d75df17cc03cca8f11b470940
maciejek052 <maciejek052@gmail.com>
2021-06-03 17:57:58 +0200

    ustawienie mojego maila na probe

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "youremail@foo.com",
+      :to => "maciejek052@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,5 +131,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  
+
 end
