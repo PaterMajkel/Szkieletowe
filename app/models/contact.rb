af37a8737040ca629d3dba677bd9388c03a7ddf8
maciejek052 <maciejek052@gmail.com>
2021-06-04 00:05:45 +0200

    Próba zrobienia kontaktu na heroku

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       subject: "My Contact Form",
-      to: "maciejek052@gmail.com",
+      to: "bigboundlecompany@gmail.com",
       from: %("#{name}" <#{email}>)
     }
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,18 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-
+  config.action_mailer.default_url_options = { :host => 'szkieletory.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'szkieletory.herokuapp.com',
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
 end
