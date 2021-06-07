96740a45e1225ae2edb4c156f13883ef31061881
Pater GitHelp <micbo26@gmail.com>
2021-06-07 23:44:19 +0200

    naprawa produktów w panelu
    PROSZĘ SIĘ OGARNĄĆ

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,5 @@ class Product < ApplicationRecord
   has_many :users, :through=> :user_product
   has_many :product_shop, :dependent => :destroy
   has_many :shops, :through=> :product_shop
-  has_many :category_product, :dependent => :destroy
-  has_many :categories, :through => :category_products
   paginates_per 6
 end
