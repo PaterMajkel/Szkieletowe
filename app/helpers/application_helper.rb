83ac864af5dbac987f989ee03b3daf5e20e0003d
Pater GitHelp <micbo26@gmail.com>
2021-06-07 23:13:34 +0200

    Revert "Merge branch 'main' of https://github.com/PaterMajkel/Szkieletowe into main"
    This reverts commit f8379e90d4597802460b19ff161778f0bcf1d788, reversing
    changes made to b55325cecd9bf70a2997da292b07d648cd6e46e6.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    if column != sort_column
+      css_class = nil
+    elsif sort_direction == "asc"
+      css_class = "fa fa-chevron-up"
+    else
+      css_class = "fa fa-chevron-down"
+    end
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to "#{title} <span class='#{css_class}'></span>".html_safe, {:sort => column, :direction => direction}
+  end
 end
