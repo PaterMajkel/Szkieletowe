3ace11f612a1aaab6becb16da64ca23e103873db
Pater GitHelp <micbo26@gmail.com>
2021-06-12 13:49:28 +0200

    teraz tylko produkty maja 0/1 kategorie

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  has_many :product_category, :dependent => :destroy
-  has_many :categories, :through=> :product_category
+  has_one :product_category, :dependent => :destroy
+  has_one :category, :through=> :product_category
   has_many :user_product, :dependent => :destroy
   has_many :users, :through=> :user_product
   has_many :product_shop, :dependent => :destroy
