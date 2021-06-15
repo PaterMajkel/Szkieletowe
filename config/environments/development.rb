da7a7fc3a7b177287d39e436c5dcd4a21e6d3e1b
Filip2137 <filip.andrejuk@gmail.com>
2021-06-15 21:57:04 +0200

    Revert "Wygląda w miarę ok"
    This reverts commit 776a891d20910014b00694fc1271e8b1720f93fd.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'bigboundlecompany',
-    password:             'PapiezJP2GMD2137',
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
