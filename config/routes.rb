fe6bb6b7505bf62ec243ce1b3fcc33a82e5b1e84
maciejek052 <maciejek052@gmail.com>
2021-06-16 17:36:38 +0200

    literowka

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/404' , to: 'errors#not_found'
   get '/422' , to: 'errors#unacceptable'
-  get '/500' , to: 'errors#server_errors'
+  get '/500' , to: 'errors#server_error'
 end
