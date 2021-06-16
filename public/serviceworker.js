178568580bf0f3294440132d74fc9cb5c2e774d6
Pater GitHelp <micbo26@gmail.com>
2021-06-16 14:07:59 +0200

    productshop co godzinę

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,7 @@
 self.addEventListener("push", (event) => {
-    let title = (event.data && event.data.text()) || "Yay a message";
-    let body = "Sprawdź teraz!";
+    var mess = event.data.text().split('|')
+    let title = (event.data && mess[0]) || "Yay a message";
+    let body = (event.data && mess[1]) || "Sprawdź teraz!";
     let tag = "push-simple-demo-notification-tag";
     let icon = '/assets/my-logo-120x120.png';
 
